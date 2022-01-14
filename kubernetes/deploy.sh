73e03d431de0fd5e9007a39d22448ac4c5d6cb14
root <joseantoniosousaborges@gmail.com>
2022-01-14 02:20:59 -0300

    ajustes

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -11,17 +11,12 @@ docker build -t app-devops:1.0 .
 docker tag app-devops:1.0 borgito/app-devops:1.0
 docker push borgito/app-devops:1.0
 
-
 cd ..
 
 # - Define Dnsname
 ip_ingress=$(minikube ip)
 
 sudo -- bash -c  "echo $ip_ingress app-desafio-dev.local >> /etc/hosts"
-
-
-
-
 
 
 # - Deploy helm
